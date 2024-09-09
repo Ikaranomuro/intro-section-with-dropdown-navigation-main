9c9b15cbec0286af06c489810201976543bd6163
Iago Oliveira Fonseca <iagooliveirafonseca83@gmail.com>
2024-09-09 19:49:43 -0300

    Iniciando a versão Mobile

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function pageClient(){
+    const mainImage = document.getElementsByClassName('main__Image')[0];
+    const body = document.getElementsByTagName('body')[0];
+    if(body.clientWidth <= 400){
+        mainImage.innerHTML = ' <img src="./images/image-hero-mobile.png" alt="" class="main__Image">'
+        console.log(mainImage)
+    } else{
+        console.log("não há nada para adicionar")
+    }
+}
+
+
 function dropdownMenuOpenOne() {
     const menuDropdownOne = document.getElementsByClassName('nav__Menu__Links__DropdownList')[0];
     const arrowUp = document.getElementsByClassName('arrowUp')[0];
